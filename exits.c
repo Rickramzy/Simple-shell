93d4e5282430004a9d4a9b6dbe6a682586edb4a5
Rickramzy <oguejioforrapheal@rocketmail.com>
2023-08-16 23:52:54 -0700

    getenv.c

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - this copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source strings
+ *@n: the amount of characters to be copied
+ *Return: the concatenated strings
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first strings
+ *@src: the second strings
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated strings
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character
+ *@s: string
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
